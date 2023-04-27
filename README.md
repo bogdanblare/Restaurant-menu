fb1ff2eb9e379d256160f399f3652b0d6d53513a
Bogdan <bicharaa10@gmail.com>
2023-04-27 17:50:13 +0300

    main

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Restaurant Page
  Project: Restaurant Page
-<div align="center"><img src="" alt="screenshot of website" width="900" /></div>
+<div align="center"><img src="https://github.com/bogdanblare/Restaurant-Page/blob/main/qrcode.png" alt="screenshot of website" width="900" /></div>
--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log(`helloworld`)
+function displayTime() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+
+    let timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = timeString;
+}
+
+displayTime();
+setInterval(displayTime, 1000);
